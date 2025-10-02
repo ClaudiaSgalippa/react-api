ecf03f6c1b5c950f8110a77077dc79e7fcacc3d7
ClaudiaSgalippa <claudia.sgappy@gmail.com>
2025-10-02 17:09:26 +0200

    Comment Milestone 1

--- src/components/MyActresses.jsx
+++ src/components/MyActresses.jsx
@@ -1,16 +1,17 @@
-import { useState, useEffect } from "react"; {/*Creazione della variabile di stato*/}
+import { useState, useEffect } from "react"; 
 
-export default function ActressesList() { 
-    const [actressesList, setActressesList] = useState ([]); {/*Array vuoto per inserire i dati di results*/}
+export default function ActressesList() { {/*Creazione della variabile di stato*/}
+    const [actressesList, setActressesList] = useState ([]); {/*Array vuoto per inserire i dati dalla chiamata*/}
 
-    function fetchActresses () {
+    function fetchActresses () { {/*Chiamata AXIOS, se la risposta sarà positiva inserisci l'array richiamato, se negativa dai errore in console.log*/}
         axios.get("https://lanciweb.github.io/demo/api/actresses/")
-            .then((res) => console.log(res.data)) 
+            .then((res) => setActressesList(res.data)) 
+            .catch((error) => console.log(error))
     }
 
-    useEffect (() => {
+    useEffect (() => { {/*Utilizzo del useEffect per invocare la funzione*/}
         fetchActresses ();
-    }, []);
+    }, []); {/*Essendo una dipendenza di stato vuota, la funzione partirà soltanto all'avvio del componente (MOUNTING)*/}
 
     return (
         <div>
