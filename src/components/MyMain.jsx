9d84f9b812d990f886d59e4707862506f9f98d94
ClaudiaSgalippa <claudia.sgappy@gmail.com>
2025-10-02 13:36:27 +0200

    first commit

---
+++ src/components/MyMain.jsx
@@ -0,0 +1,14 @@
+const MyMain = () => {
+
+    return (
+        <>
+            <main>
+                <div className="onlyHeader">
+                    <h1>MyMain</h1>
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default MyMain
